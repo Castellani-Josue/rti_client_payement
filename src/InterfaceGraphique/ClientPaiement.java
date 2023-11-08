562732f3b2862ae7784dd7af9df75a1e457f55c5
Josué Castellani <josue.castellani@student.hepl.be>
2023-11-08 13:57:27 +0100

    0811202313H57

--- src/InterfaceGraphique/ClientPaiement.java
+++ src/InterfaceGraphique/ClientPaiement.java
@@ -1,16 +1,83 @@
 package InterfaceGraphique;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 
-public class ClientPaiement extends JFrame {
-    private JPanel contentPane;
+public class ClientPaiement extends JFrame
+{
 
+    private static ClientPaiement dialog;
+    private JPanel MainPanel;
+    private JPanel RecherchePanel;
+    private JPanel FacturePanel;
+    private JLabel NumClientLabel;
+    private JTextField NumCliTxt;
+    private JLabel ResultatLabel;
+    private JTable ResultatTable;
+    private JScrollPane ResultatScroll;
+    private JPanel PaiementPanel;
+    private JButton SelectionnerButton;
+    private JButton payerButton;
+    private JLabel NomCliLabel;
+    private JLabel VisaLabel;
+    private JTextField NomCliTxt;
+    private JTextField VisaTxt;
 
 
     public ClientPaiement()
     {
-        setContentPane(contentPane);
+
+
+        //Dimension num = new Dimension(50,20);
+        //NumCliTxt.setPreferredSize(num);
+        Border border = new LineBorder(Color.BLACK, 1);
+        NumCliTxt.setBorder(border);
+        NomCliTxt.setBorder(border);
+        VisaTxt.setBorder(border);
+
+        /*int width = ResultatTable.getWidth();
+        int newHeight = 300;
+        Dimension panier = new Dimension(width,newHeight);
+
+        ResultatScroll.setPreferredSize(panier);*/
+
+        Dimension nomCli = new Dimension(160,20);
+        NomCliTxt.setPreferredSize(nomCli);
+        VisaTxt.setPreferredSize(nomCli);
+
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Facture ID");
+        model.addColumn("Id Client");
+        model.addColumn("Date");
+        model.addColumn("Montant (€)");
+        model.addColumn("Payé");
+
+        ResultatTable.setModel(model);
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+
+        // Supposons que "columnIndex" est l'index de la colonne pour laquelle vous souhaitez définir l'alignement.
+        ResultatTable.getColumnModel().getColumn(0).setCellRenderer(centerRenderer);
+        ResultatTable.getColumnModel().getColumn(1).setCellRenderer(centerRenderer);
+        ResultatTable.getColumnModel().getColumn(2).setCellRenderer(centerRenderer);
+
+        ResultatTable.getTableHeader().setReorderingAllowed(false);
+
+        setTitle("Client Paiement");
+        setContentPane(MainPanel);
+        setLocationRelativeTo(null);
+        pack();
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setSize(1000,700);
+        setLocation((screen.width - this.getSize().width)/2,(screen.height - this.getSize().height)/2);
+        setResizable(false);
 
 
         /*buttonOK.addActionListener(new ActionListener() {
@@ -24,10 +91,10 @@ public class ClientPaiement extends JFrame {
 
 
 
-    public static void main(String[] args) {
-        ClientPaiement dialog = new ClientPaiement();
-        dialog.pack();
+    public static void main(String[] args)
+    {
+        FlatDarculaLaf.install(new FlatDarculaLaf());
+        dialog = new ClientPaiement();
         dialog.setVisible(true);
-        System.exit(0);
     }
 }
